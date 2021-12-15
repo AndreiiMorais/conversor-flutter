ba04269caeb16804e87230d3e466781db986461c
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-15 14:19:40 -0300

    bugged

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,3 +1,7 @@
+import 'dart:html';
+import 'dart:ui';
+
+import 'package:conversor_flutter/src/models/currency.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +9,40 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(children: [
+          Image.asset('asset/logo.png'),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Row(
+              children: [
+                DropdownButton(
+                  items: const [
+                    DropdownMenuItem(
+                      value: 1,
+                      child: Text('Real'),
+                    ),
+                    DropdownMenuItem(
+                      value: 2,
+                      child: Text('Dolar'),
+                    ),
+                  ],
+                  onChanged: (value) {},
+                ),
+                const TextField(),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('CONVERTER'),
+          )
+        ]),
+      ),
+    );
   }
 }
