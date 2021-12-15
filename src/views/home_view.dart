2c83263897de1fbd62c524700ecc7240cd50123f
Andrei Silveira Morais <andrei.morais@outlook.com>
2021-12-15 11:45:27 -0300

    update

---
+++ src/views/home_view.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'src/views/home_view.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
